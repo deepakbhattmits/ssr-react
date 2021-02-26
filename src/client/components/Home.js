1771c864a01578525c270fdcb33952ca01cec399
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-27 00:07:26 +0530

    Create Home.js

---
+++ src/client/components/Home.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div>
+			<h1>I am best home component</h1>
+			<button onClick={()=>{console.log('Pressing button')}}>Press me!</button>
+		</div>
+	);
+};
+export default Home;
