f62fc393db9ccebb54cc32e8aa8dbd2f379e62a5
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-27 00:07:19 +0530

    Create renderer.js

---
+++ src/helpers/renderer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {renderToString} from 'react-dom/server';
+import Home from '../client/components/Home';
+export const renderer =()=>{
+    const content = renderToString(<Home />);
+	return `<html>
+	<head></head>
+	<body>
+	<div id="root">${content}</div>
+	<script src="bundle.js"> </script>
+	</body>
+	</html>`
+}
